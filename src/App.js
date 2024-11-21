7b8033cccb4435ed082c64def0517bf2385c231a
vatsalSabalpara18 <vatsalsabalpara18@gmail.com>
2024-11-21 08:44:10 +0530

    components

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React (folder stucture)
+          Learn React (components)
         </a>
       </header>
     </div>
